b7bc4241f6a5027692877142ef6394841f3fe928
Lincoln de Sousa <lincoln@comum.org>
2010-08-04 18:29:32 -0300

    Movendo o header para o header.php e o footer para o footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <div id="footer" class="portalWidth">
+      <ul id="parceiros">
+        <li></li>
+      </ul>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Projeto de Formação Gesac</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <div id="header" class="portalWidth">
-      <h1 class="logo"><a href="#">Projeto de Formação Gesac</a></h1>
-
-      <ul id="topMenu">
-        <li><a href="#">Apresentação</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3 dolor sit amet</a></li>
-        <li><a href="#">Link 4 lorem ipsum</a></li>
-        <li><a href="#">Calendário formação</a></li>
-      </ul>
-
-      <form id="searchForm" action="#" method="post">
-        <input type="text" name="q" />
-        <input type="button" value="buscar" />
-      </form>
-    </div>
+<?php get_header (); ?>
 
     <div id="buttonBar">
       <div class="portalWidth">
@@ -157,10 +134,4 @@
       </div>
     </div>
 
-    <div id="footer" class="portalWidth">
-      <ul id="parceiros">
-        <li></li>
-      </ul>
-    </div>
-  </body>
-</html>
+<?php get_footer (); ?>
