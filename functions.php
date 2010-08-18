0dc6e8531cfeb32eec1321f7cee3fbbaefa8edba
Ronaldo Amaral <rsantos@iff.edu.br>
2010-08-18 19:20:56 -0300

    Adicionado colunas(esquerda, central, direita) ao portal

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
   if ( function_exists('register_sidebar') )
-    register_sidebar(array('name' => "left-column"));
-    register_sidebar(array('name' => "center-column"));
+    register_sidebar(array('name' => "left-column-top"));
+    register_sidebar(array('name' => "left-column-bottom"));
+    register_sidebar(array('name' => "center-column-top"));
+    register_sidebar(array('name' => "center-column-bottom"));
     register_sidebar(array('name' => "right-column"));
 ?>
 
--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 
   <div id='mainContent'>
     <div id="left-column">
-	<?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("left-column") ) : ?>
-	<?php endif ?>
+      <div id="left-column-top">
+	  <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("left-column-top") ) : ?>
+	  <?php endif ?>
+      </div>
+      <div id="left-column-bottom">
+	  <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("left-column-bottom") ) : ?>
+	  <?php endif ?>
+      </div>
     </div>
 
     <div id="center-column">
-	<?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("center-column") ) : ?>
-	<?php endif ?>
+      <div id="center-column-top">
+	  <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("center-column-top") ) : ?>
+	  <?php endif ?>
+      </div>
+
+      <div id="center-column-bottom">
+	  <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("center-column-bottom") ) : ?>
+	  <?php endif ?>
+      </div>
     </div>
     
     <div id="right-column">
