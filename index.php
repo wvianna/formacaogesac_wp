ead01f5a9441a91735460a2301c6ee18a4c2324d
Equipe NSI IFF Campos <fgesac@gmail.com>
2010-09-02 19:46:07 -0300

    Alterações no CSS e index.php

--- index.php
+++ index.php
@@ -4,27 +4,10 @@
   <div id="content" class="grid_9" style="">
     <div id="left-column" class="grid_5 alpha">
       <div id="left-column-top">
-      
-      <div class="hlTabs">
-              <ul class="hlNav">
-            	<li><a href="#hl-1" title="">&#8226;</a></li>
-                <li><a href="" title="">&#8226;</a></li>
-                <li><a href="" title="">&#8226;</a></li>
-                <li><a href="" title="">&#8226;</a></li>
-                <li><a href="" title="">&#8226;</a></li>
-            </ul>
-            <!-- loop -->
-                <div id="hl-1">
-				<a class="thumb" href=""><img src="<?php bloginfo('template_url');?>/imgs/_eraseme2.jpg" alt="alt" /></a>
-                <h2><a href="#">Título da Matéria</a></h2>
-                <p>Nulla pellentesque pellentesque commodo. Pellentesque sed mauris vel nisi interdum bibendum et a arcu...</p>
-                </div>
-            <!-- /loop -->
-        	
-		</div>
- 
-      
-	  <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("left-column-top") ) : ?>
+
+      <?php include (ABSPATH . '/wp-content/plugins/featured-content-gallery/gallery.php'); ?>
+	  
+          <?php  if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("left-column-top") ) : ?>
 	  <?php endif ?>
       </div>
       <div id="left-column-bottom">  
@@ -53,7 +36,7 @@
     <li id="right_pontos"><a href="#">Pontos Atendidos</a></li>
     <li id="right_equipe"><a href="#">Conheça a Equipe</a></li>
     <li id="right_biblioteca"><a href="#">Biblioteca Digital</a></li>
-    <li id="right_saladeaula"><a href="#">Sala de Aula</a></li>
+    <li id="right_saladeaula"><a href="http://www.formacao.gesac.gov.br/ead/">Sala de Aula</a></li>
     <li id="right_redesocial"><a href="#">Rede Social</a></li>
     <li id="right_fotosevideos"><a href="#">Fotos e Vídeos</a></li>
     <li id="right_servicospublicos"><a href="#">Serviços Públicos</a></li>
