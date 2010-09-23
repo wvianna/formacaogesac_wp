b8fa718dea3f0c355d0b8c614346150736eb5e03
Equipe NSI IFF Campos <fgesac@gmail.com>
2010-09-23 20:05:25 -0300

    inclusão de informações dos pontos atendidos

--- pontosatendidos.php
+++ pontosatendidos.php
@@ -7,8 +7,15 @@ Template Name: Pontos Atendidos
   <div id="mainContent" class="container_11">
   <div id="content" class="grid_9" style="">
     <div id="pageContent">
+<p>O Ministério das Comunicações escolheu 739 Pontos Gesac para participação na primeira fase do Projeto. Essa seleção poderá ser alterada durante a execução do Projeto, de acordo com os seguintes critérios:<br><br>
 
-<h1>Pontos atendidos</h1>
+ - desativação do Ponto;<br>
+ - outras situações, conforme disponibilidade de recursos, que ficam sujeitas à avaliação do Ministério.<br><br>
+
+Por tratar-se de um projeto piloto, nesta fase inicial que vai até maio de 2011, não haverá inscrições de novos Pontos para a participação no Projeto.<br><br>
+
+<b>Veja abaixo a lista dos Pontos atendidos:</b></p>
+
 <?php
 echo'  
   <label>
